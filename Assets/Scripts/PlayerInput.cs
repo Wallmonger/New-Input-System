3276c0880359e0e2be0a4bda72be0716920d413d
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-23 20:24:53 +0200

    Second commit

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -7,7 +7,7 @@ public class PlayerInput : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Hello
     }
 
     // Update is called once per frame
