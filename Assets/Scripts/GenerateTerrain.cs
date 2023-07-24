236193b7917a1114fb0dd13cb6fc282b1911b55a
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-24 18:03:45 +0200

    PerlinNoise

--- Assets/Scripts/GenerateTerrain.cs
+++ Assets/Scripts/GenerateTerrain.cs
@@ -20,15 +20,18 @@ public class GenerateTerrain : MonoBehaviour
 
     public TerrainData GenTdata(TerrainData tData)
     {
-        tData.heightmapResolution = size;
-        tData.size = new Vector3(size, height, size);
-        float[,] h = new float[size, size];
+        tData.heightmapResolution = size;                       // Définit la résolution de la carte de hauteur
+        tData.size = new Vector3(size, height, size);           // Taille du terrain en Vector3
+        float[,] h = new float[size, size];                     // Création d'un array en deux dimensions [,] pour stocker les hauteurs de terrain
 
+
+        // On remplit le tableau avec des valeurs de bruit de Perlin pour générer
         for (int i = 0; i < size; i++)
         {
             for (int j = 0; j < size; j++)
             {
-
+                // Génération d'une hauteur aléatoire et enregistrement dans l'array
+                h[i, j] = Mathf.PerlinNoise((float)i / size * 10, (float)j / size * 10);
             }
         }
         return tData;
